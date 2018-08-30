dbd735bbe6b29b4eec6753f77af479a4e23794ba
Francesco D'Abbraccio <fmeater@gmail.com>
2018-08-31 01:14:10 +0200

    Sign Up page + refactor

--- app/src/main/java/com/example/francesco/mytravel/MainActivity.java
+++ app/src/main/java/com/example/francesco/mytravel/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.francesco.mytravel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSignUpActivity(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
