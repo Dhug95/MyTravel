2c945b27fc80d1af4f1a91ea2b31ef2351498f5f
Francesco D'Abbraccio <fmeater@gmail.com>
2018-09-05 13:13:00 +0200

    Reorganized project in packages

--- app/src/main/java/com/example/francesco/mytravel/utils/TripListAdapter.java
+++ app/src/main/java/com/example/francesco/mytravel/utils/TripListAdapter.java
@@ -1,14 +1,15 @@
-package com.example.francesco.mytravel;
+package com.example.francesco.mytravel.utils;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.example.francesco.mytravel.R;
+import com.example.francesco.mytravel.fragments.TripsFragment;
+import com.example.francesco.mytravel.tasks.GetTripPage;
 
 import java.util.LinkedList;
 
